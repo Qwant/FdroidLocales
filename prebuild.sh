5ab26cae81a827ac92222a2459fc2bac9432fb88
Manizuca <matias.nicolas.zc@gmail.com>
2015-01-19 21:25:38 -0300

    More updates for Fx36

--- prebuild.sh
+++ prebuild.sh
@@ -8,7 +8,7 @@ rm -R toolkit/crashreporter/google-breakpad/src/client/
 rm -R toolkit/crashreporter/google-breakpad/src/processor/testdata/
 rm -R toolkit/crashreporter/google-breakpad/src/third_party/linux/
 
-find content/html/content/test/ -type f -exec rm '{}' \;
+find dom/html/test/ -type f -exec rm '{}' \;
 
 rm -R accessible/tests/
 rm -R addon-sdk/source/test/
@@ -17,19 +17,20 @@ rm -R browser/devtools/webide/test/
 rm -R build/mobile/robocop/
 rm -R build/mobile/sutagent/
 rm -R chrome/test/
-rm -R content/base/crashtests/
-rm -R content/base/test/
-rm -R content/test/
-rm -R browser/devtools/webide/test/
+rm -R content/
 rm -R docshell/test/
 rm -R dom/apps/tests/
+rm -R dom/base/crashtests/
+rm -R dom/base/test/
+rm -R dom/html/test/
+rm -R dom/indexedDB/test/
 rm -R dom/tests/
 rm -R layout/base/crashtests/
 rm -R layout/generic/crashtests/
 rm -R layout/generic/test/
 rm -R layout/reftests/
 rm -R mobile/android/base/tests/
-rm -R toolkit/modules/tests/
+rm -R mobile/android/build/classycle/
 rm -R modules/libmar/tests/
 rm -R modules/libjar/test/
 rm -R modules/libjar/zipwriter/test/
@@ -39,12 +40,13 @@ rm -R security/manager/ssl/tests/*test/
 rm -R security/nss/cmd/bltest/tests/
 rm -R security/nss/tests/
 rm -R services/sync/tests/
-rm -R testing/mozbase/mozinstall/tests/
+rm -R testing/crashtest/
 rm -R testing/mozbase/mozinstall/tests/
 rm -R testing/mozbase/mozprofile/tests/
-rm -R testing/crashtest/
+rm -R testing/web-platform/
 rm -R toolkit/devtools/apps/tests/
 rm -R toolkit/components/search/tests/
+rm -R toolkit/modules/tests/
 rm -R toolkit/mozapps/extensions/test/
 rm -R toolkit/mozapps/update/tests/
 rm -R toolkit/webapps/tests/
@@ -59,11 +61,14 @@ sed -i -e '/nsExceptionHandler/d' toolkit/xre/nsAndroidStartup.cpp
 sed -i -e '/nsExceptionHandler/d' toolkit/xre/nsEmbedFunctions.cpp
 
 sed -i -e '/source\/test\//d' addon-sdk/moz.build
+sed -i -e '/testing\/web-platform\/mach_commands.py/d' build/mach_bootstrap.py
 sed -i -e '/tests\//d' dom/apps/moz.build
-sed -i -e '/test\//d' content/html/content/moz.build
-sed -i -e '/xpcshell.ini/d' content/moz.build
+sed -i -e '/test\//d' dom/html/moz.build
+sed -i -e '/test\//d' dom/indexedDB/moz.build
 sed -i -e '/MOCHITEST/d' layout/generic/moz.build
 sed -i -e '/reftests\//d'  -e '/crashtest\//d' layout/moz.build
+sed -i -e '/classycle_jar/,+7d' -e 's/.geckoview.deps ././g' -e 's/PROGUARD_PASSES=1/PROGUARD_PASSES=3/g' mobile/android/base/Makefile.in
+sed -i -e '/\/content/d' toolkit/toolkit.mozbuild
 sed -i -e '/xpcshell.ini/d' toolkit/components/search/moz.build
 sed -i -e '/tests\//d' toolkit/modules/moz.build
 sed -i -e '/tests/d' toolkit/mozapps/update/moz.build
@@ -75,3 +80,15 @@ echo 'MOZ_DEVICES=0\nMOZ_NATIVE_DEVICES=0\nMOZ_ANDROID_GOOGLE_PLAY_SERVICES=0\n'
 echo "mk_add_options 'export MOZ_CHROME_MULTILOCALE=$(tr '\n' ' ' <  $REPO/used-locales)'" >> .mozconfig
 echo "mk_add_options 'export L10NBASEDIR=$REPO'" >> .mozconfig
 echo "ac_add_options --with-l10n-base=$REPO" >> .mozconfig
+
+##Fix Bug #1083116
+patch -p1 <$REPO/Bug1083116.patch
+
+sed -i -e 's/size_impl(v/size_impl(const v/g' memory/mozjemalloc/jemalloc.c
+
+##Fix Bug #1091377
+sed -i -e '/MOZ_WIDGET_TOOLKIT/,+2d' build/stlport/moz.build
+
+##Get rid of Gradle
+rm -R mobile/android/gradle/
+sed -i -e '/gradle/d' mobile/android/moz.build
