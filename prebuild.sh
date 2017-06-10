405d27ebb97172a260522e4937b9ca80bd400bb8
Matías Zúñiga <matias.nicolas.zc@gmail.com>
2017-06-10 01:22:44 -0400

    Update patches for Fennec 54

--- prebuild.sh
+++ prebuild.sh
@@ -9,7 +9,8 @@ rm -R toolkit/crashreporter/google-breakpad/src/third_party/linux/
 
 sed -i -e '/nsExceptionHandler/d' gfx/ipc/GPUParent.cpp
 sed -i -e '/nsExceptionHandler/d' ipc/glue/GeckoChildProcessHost.cpp
-sed -i -e '/nsExceptionHandler/d' -e '/AnnotationTable/d'  ipc/glue/CrashReporterHost.h
+sed -i -e 's/CrashReporter::ThreadId/int/g' ipc/glue/CrashReporterHost.cpp
+sed -i -e '/nsExceptionHandler/d' -e '/AnnotationTable/d' -e 's/CrashReporter::ThreadId/int/g' ipc/glue/CrashReporterHost.h
 sed -i -e '/nsExceptionHandler/d' -e '/AnnotationTable/d' ipc/glue/CrashReporterMetadataShmem.h
 sed -i -e '/mNotes\.Iter/,+6d' -e '/mNotes\.Put/d' ipc/glue/CrashReporterMetadataShmem.cpp
 sed -i -e '/nsExceptionHandler/d' toolkit/xre/nsAndroidStartup.cpp
@@ -26,7 +27,6 @@ rm -R chrome/test/
 rm -R devtools/client/debugger/test/
 rm -R devtools/client/webide/test/
 rm -R docshell/test/
-rm -R dom/apps/tests/
 rm -R dom/base/crashtests/
 rm -R dom/base/test/
 rm -R dom/html/test/
@@ -52,6 +52,7 @@ rm -R security/nss/cmd/bltest/tests/
 rm -R security/nss/cmd/samples/
 rm -R security/nss/tests/
 rm -R services/sync/tests/
+rm -R servo/tests/unit/
 rm -R other-licenses/nsis/nsisui.exe
 rm -R testing/crashtest/
 rm -R testing/mozbase/mozinstall/tests/
@@ -59,6 +60,7 @@ rm -R testing/mozbase/mozprofile/tests/
 rm -R testing/mozbase/mozrunner/mozrunner/resources/metrotestharness.exe
 rm -R testing/talos/talos/
 rm -R testing/web-platform/
+rm -R third_party/rust/term/tests/data/
 rm -R tools/update-packaging/test/
 rm -R toolkit/components/downloads/test/unit/
 rm -R toolkit/components/mediasniffer/test/unit/
@@ -73,6 +75,11 @@ rm -R xpcom/tests/
 cp -f $REPO/.mozconfig ./
 cp -f $REPO/used-locales ./
 
+sed -i -e '/BROWSER_CHROME_MANIFESTS/s/ManifestparserManifestList/OrderedStringList/' python/mozbuild/mozbuild/frontend/context.py
+sed -i -e '/PYTHON_UNITTEST_MANIFESTS/s/ManifestparserManifestList/OrderedStringList/' python/mozbuild/mozbuild/frontend/context.py
+sed -i -e "/'testing\/mochitest'/d" python/mozbuild/mozbuild/testing.py
+sed -i -e '/PYTHON_UNITTEST/d' python/mozbuild/mozbuild/testing.py
+
 sed -i -e '/tests\//d' accessible/moz.build
 sed -i -e '/source\/test\//d' -e '/GENERATED_FILES += addons/,+5d' addon-sdk/moz.build
 sed -i -e '/testing\/web-platform\/mach_commands.py/d' build/mach_bootstrap.py
@@ -80,7 +87,7 @@ sed -i -e '/TESTS_MANIFESTS/,+36d' docshell/moz.build
 sed -i -e '/tests\//d' dom/file/moz.build
 sed -i -e '/test\//d' dom/html/moz.build
 sed -i -e '/TEST_DIRS/,+14d' dom/indexedDB/moz.build
-sed -i -e '/TEST/d' dom/xhr/moz.build
+sed -i -e '/tests/d' dom/xhr/moz.build
 sed -i -e '/MOCHITEST/d' layout/generic/moz.build
 sed -i -e '/reftest/d'  -e '/crashtest/d' layout/moz.build
 sed -i -e '/classycle_jar/,+7d' -e 's/.geckoview.deps ././g' -e 's/PROGUARD_PASSES=1/PROGUARD_PASSES=3/g' mobile/android/base/Makefile.in
